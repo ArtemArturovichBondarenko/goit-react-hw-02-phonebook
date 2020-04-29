6bdd3604743692d4d85bcc7390dcb83c47334a0a
Artem Bondarenko <artem.arturovich.bondarenko@gmail.com>
2020-04-29 23:38:50 +0300

    Updates

---
+++ precache-manifest.a781849e0eb044d5454c33425653e4be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e55cb56bec2192c6dfd3",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.572492ee.chunk.css"
+  },
+  {
+    "revision": "e55cb56bec2192c6dfd3",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.ea750fae.chunk.js"
+  },
+  {
+    "revision": "259b50e1541406add527",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime~main.bf2c92ed.js"
+  },
+  {
+    "revision": "299a0e07c605d02d4b32",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.ff0efd35.chunk.js"
+  },
+  {
+    "revision": "175491b821386b255bc0426bd29f27e7",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  }
+];
